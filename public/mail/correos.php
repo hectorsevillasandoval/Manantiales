f9334115eb8e3496e9cd45fd28053053a127f21e
unknown <hsevilla@WSMWEB005.whitesharkmedia.net>
2015-03-11 10:35:23 -0600

    Desactivada cuenta de prueba

--- public/mail/correos.php
+++ public/mail/correos.php
@@ -8,8 +8,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'barcadict@gmail.com';                 // SMTP username
-$mail->Password = '15721sshd';                           // SMTP password
+$mail->Username = 'correo';                 // SMTP username
+$mail->Password = 'clave';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
